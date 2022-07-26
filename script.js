0b2c0f8adb5c66adf833afe4e05c2981aeccce48
Mohammed Ajmal <68717489+mohammedajmalgit@users.noreply.github.com>
2022-07-26 15:28:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', ()=>{
+    if(currentActive < circles.length){
+        currentActive++;
+        console.log(currentActive)
+    }
+    update()
+})
+
+prev.addEventListener('click', ()=>{
+    if(currentActive > 1){
+        currentActive--;
+        console.log(currentActive);
+    }
+    update()
+})
+
+function update(){
+    circles.forEach((circle, idx)=>{
+        if(idx<currentActive){
+            circle.classList.add('active')
+        }else{
+            circle.classList.remove('active')
+        }
+    })
+
+    progress.style.width = (currentActive-1)/(circles.length-1)*100 +'%'
+
+    if(currentActive == 1){
+        prev.disabled = true
+    }else if(currentActive === circles.length){
+        next.disabled = true
+    }else{
+        prev.disabled = false
+        next.disabled = false
+    }
+}
